e14688fa77588478561748252afde5a3389cfe77
huongle11 <huongltt.22it@vku.udn.vn>
2025-06-18 19:54:13 +0700

    Dựng khung DefaultLayout

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,6 +1,16 @@
+import classNames from "classnames/bind";
+import styles from "./Header.module.scss";
+const cx = classNames.bind(styles);
 // Dùng chung Header cho các Layout khác
 function Header() {
-  return <h1> Header </h1>;
+  return (
+    <header className={cx("wrapper")}>
+      <div className={cx("inner")}>
+        {/* {Logo} */}
+        {/* {Search} */}
+      </div>
+    </header>
+  );
 }
 
 export default Header;
